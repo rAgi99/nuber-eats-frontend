888445992ebc83219e0375b54955766ffc823c07
rAgi99 <gunmo2929@naver.com>
2022-06-24 21:01:03 +0900

    Header part two

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,25 +1,14 @@
-import { gql, useQuery } from "@apollo/client";
 import React from "react";
-import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Header } from "../components/header";
+import { useMe } from "../hooks/userMe";
+import { NotFound } from "../pages/404";
 import { Restaurants } from "../pages/client/restaurants";
-import { meQuery } from "../__generated__/meQuery";
 
 const ClientRoutes = [<Route path="/*" element={<Restaurants />} />];
 
-const ME_QUERY = gql`
-  query meQuery {
-    me {
-      id
-      email
-      role
-      verified
-    }
-  }
-`;
-
 export const LoggedInRouter = () => {
-  const { data, loading, error } = useQuery<meQuery>(ME_QUERY);
+  const { data, loading, error } = useMe();
   if (!data || loading || error) {
     return (
       <div className="h-screen flex justify-center items-center">
@@ -32,7 +21,7 @@ export const LoggedInRouter = () => {
       <Header />
       <Routes>
         {data.me.role === "Client" && ClientRoutes}
-        <Route path="/potato" element={<Navigate replace to="/" />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
