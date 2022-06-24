e66592833fefc9bdd7432f1e11553d0e83d160db
rAgi99 <gunmo2929@naver.com>
2022-06-24 21:06:51 +0900

    .

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
 import { Header } from "../components/header";
 import { useMe } from "../hooks/userMe";
-import { NotFound } from "../pages/404";
 import { Restaurants } from "../pages/client/restaurants";
 
 const ClientRoutes = [<Route path="/*" element={<Restaurants />} />];
