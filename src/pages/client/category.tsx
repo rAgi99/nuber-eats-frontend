f317f2085a0ddb0c8246a9ea89feafeedbba7c85
rAgi99 <gunmo2929@naver.com>
2022-06-27 17:09:23 +0900

    Search Page, Category Page show up Restaurant

--- src/pages/client/category.tsx
+++ src/pages/client/category.tsx
@@ -1,7 +1,10 @@
 import { gql, useQuery } from "@apollo/client";
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
+import { useLocation, useParams } from "react-router-dom";
+import { Restaurant } from "../../components/restaurant";
 import { CATEGORY_FRAGMENT, RESTAURANT_FRAGMENT } from "../../fragments";
+import { category, categoryVariables } from "../../__generated__/category";
 
 const CATEGORY_QUERY = gql`
   query category($input: CategoryInput!) {
@@ -23,15 +26,46 @@ const CATEGORY_QUERY = gql`
 `;
 
 export const Category = () => {
+  const location = useLocation();
   const params = useParams<{ slug: string }>();
-  const { data, loading } = useQuery(CATEGORY_QUERY, {
+  const { data, loading } = useQuery<category, categoryVariables>(CATEGORY_QUERY, {
     variables: {
       input: {
         page: 1,
-        slug: params.slug,
+        slug: params.slug + "",
       },
     },
   });
-  console.log(data);
-  return <h1>Category</h1>;
+  return (
+    <div>
+      <Helmet>
+        <title>Category | Nuber Eats</title>
+      </Helmet>
+      <div className="bg-gray-800 w-full py-40 flex items-center justify-center">
+        <span className="text-white mr-3 opacity-60 font-semibold text-3xl">
+          You're result for {location.pathname.split("/category/")} has
+        </span>
+        <span className="text-white opacity-60 font-semibold text-3xl">
+          {data?.category.restaurants?.length} items
+        </span>
+      </div>
+      <div>
+        {!loading && (
+          <div className="max-w-screen-2xl pb-20 mx-auto mt-8">
+            <div className="grid mt-16 md:grid-cols-3 gap-x-5 gap-y-10">
+              {data?.category.restaurants?.map((restaurant) => (
+                <Restaurant
+                  key={restaurant.id}
+                  id={restaurant.id + ""}
+                  coverImg={restaurant.coverImg}
+                  name={restaurant.name}
+                  categoryName={restaurant.category?.name}
+                />
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
--- src/pages/client/search.tsx
+++ src/pages/client/search.tsx
@@ -2,6 +2,7 @@ import { gql, useLazyQuery } from "@apollo/client";
 import React, { useEffect } from "react";
 import { Helmet } from "react-helmet-async";
 import { useLocation, useNavigate } from "react-router-dom";
+import { Restaurant } from "../../components/restaurant";
 import { RESTAURANT_FRAGMENT } from "../../fragments";
 import {
   searchRestaurant,
@@ -31,6 +32,7 @@ export const Search = () => {
     searchRestaurantVariables
   >(SEARCH_RESTAURANT);
   useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [_, query] = location.search.split("?term=");
     if (!query) {
       return navigate("/", { replace: true });
@@ -43,13 +45,37 @@ export const Search = () => {
         },
       },
     });
-  }, [navigate, location]);
+  }, [navigate, location, callQuery]);
   return (
     <div>
       <Helmet>
         <title>Search | Nuber Eats</title>
       </Helmet>
-      <h1>Search page</h1>
+      <div className="bg-gray-800 w-full py-40 flex items-center justify-center">
+        <span className="text-white mr-3 opacity-60 font-semibold text-3xl">
+          You're result for {location.search.split("?term=")} has
+        </span>
+        <span className="text-white opacity-60 font-semibold text-3xl">
+          {data?.searchRestaurant.restaurants?.length} items
+        </span>
+      </div>
+      <div>
+        {!loading && (
+          <div className="max-w-screen-2xl pb-20 mx-auto mt-8">
+            <div className="grid mt-16 md:grid-cols-3 gap-x-5 gap-y-10">
+              {data?.searchRestaurant.restaurants?.map((restaurant) => (
+                <Restaurant
+                  key={restaurant.id}
+                  id={restaurant.id + ""}
+                  coverImg={restaurant.coverImg}
+                  name={restaurant.name}
+                  categoryName={restaurant.category?.name}
+                />
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
