943efaa9da2503c0d13d7a6a409bac29bd3c1748
rAgi99 <gunmo2929@naver.com>
2022-06-27 11:02:30 +0900

    Restaurants Query

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -17,11 +17,11 @@ export const Header: React.FC = () => {
       <header className="py-4">
         <div className="w-full px-5 xl:px-0 max-w-screen-2xl mx-auto flex justify-between items-center">
           <Link to="/">
-            <img src={nuberLogo} alt="Nuber Eats" className="w-36" />
+            <img src={nuberLogo} alt="Nuber Eats" className="w-44" />
           </Link>
           <span className="text-xs">
             <Link to="/edit-profile">
-              <FontAwesomeIcon icon={faUser} className="text-xl" />
+              <FontAwesomeIcon icon={faUser} className="text-3xl" />
             </Link>
           </span>
         </div>
