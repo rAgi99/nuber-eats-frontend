1a56b2f3246cd1209273823b746e8cd9bbfea695
rAgi99 <gunmo2929@naver.com>
2022-06-28 15:09:38 +0900

    Testing Header and 404

---
+++ src/components/__tests__/header.spec.tsx
@@ -0,0 +1,70 @@
+import { MockedProvider } from "@apollo/client/testing";
+import { render, waitFor, screen } from "@testing-library/react";
+import { Header } from "../header";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ME_QUERY } from "../../hooks/userMe";
+
+describe("<Header />", () => {
+  it("renders verify banner", async () => {
+    render(
+      <MockedProvider
+        mocks={[
+          {
+            request: {
+              query: ME_QUERY,
+            },
+            result: {
+              data: {
+                me: {
+                  id: 1,
+                  email: "",
+                  role: "",
+                  verified: false,
+                },
+              },
+            },
+          },
+        ]}
+      >
+        <Router>
+          <Header />
+        </Router>
+      </MockedProvider>
+    );
+    await waitFor(async () => {
+      await new Promise((resolve) => setTimeout(resolve, 5));
+      screen.getByText("Please verify your email.");
+    });
+  });
+  it("renders without verify banner", async () => {
+    render(
+      <MockedProvider
+        mocks={[
+          {
+            request: {
+              query: ME_QUERY,
+            },
+            result: {
+              data: {
+                me: {
+                  id: 1,
+                  email: "",
+                  role: "",
+                  verified: true,
+                },
+              },
+            },
+          },
+        ]}
+      >
+        <Router>
+          <Header />
+        </Router>
+      </MockedProvider>
+    );
+    await waitFor(async () => {
+      await new Promise((resolve) => setTimeout(resolve, 5));
+      expect(screen.queryByText("Please verify your email.")).toBeNull();
+    });
+  });
+});
