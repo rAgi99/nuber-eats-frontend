b5b6f7f2f9983605a7efc81df96f24928f22a2cc
rAgi99 <gunmo2929@naver.com>
2022-07-01 17:21:14 +0900

    EditProfile E2E part one

---
+++ cypress/e2e/user/edit-profile.cy.ts
@@ -0,0 +1,16 @@
+describe("Edit Profile", () => {
+  const user = cy;
+  beforeEach(() => {
+    user.login("gee150@gmail.com", "121212");
+  });
+  it("can go to /edit-profile using the header", () => {
+    user.get('a[href="/edit-profile"]').click();
+    user.wait(2000);
+    user.title().title().should("eq", "Edit Profile | Nuber Eats");
+  });
+  it("can change email", () => {
+    user.visit("/edit-profile");
+    user.findByPlaceholderText(/email/i).clear().type("new@nomadcoders.co");
+    user.findByRole("button").click();
+  });
+});
