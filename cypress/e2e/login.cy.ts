43faf226037b3d542b082616b161427ae4c3d19f
rAgi99 <gunmo2929@naver.com>
2022-07-01 16:34:15 +0900

    CreateAccount E2E part one

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -16,14 +16,11 @@ describe("Log In", () => {
       .clear();
     user.findByRole("alert").should("have.text", "Password is required");
   });
-  it("can fill out the form", () => {
+  it("can fill out the form and log in", () => {
     user.visit("/");
     user.findByPlaceholderText(/email/i).type("gee150@gmail.com");
     user.findByPlaceholderText(/password/i).type("121212");
     user.findByRole("button").should("not.have.class", "pointer-events-none").click();
     user.window().its("localStorage.nuber-token").should("be.a", "string");
-  });
-  it("sign up", () => {
-    user.visit("/create-account");
   });
 });
