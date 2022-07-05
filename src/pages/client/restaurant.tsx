3a560d8375b1f22795e4ed90465d88b542c1eafd
rAgi99 <gunmo2929@naver.com>
2022-07-05 15:40:11 +0900

    Making Order parts one

--- src/pages/client/restaurant.tsx
+++ src/pages/client/restaurant.tsx
@@ -1,9 +1,10 @@
 import { gql, useQuery } from "@apollo/client";
-import React from "react";
+import React, { useState } from "react";
 import { Helmet } from "react-helmet-async";
 import { useParams } from "react-router-dom";
 import { Dish } from "../../components/dish";
 import { DISH_FRAGMENT, RESTAURANT_FRAGMENT } from "../../fragments";
+import { CreateOrderItemInput } from "../../__generated__/globalTypes";
 import { restaurant, restaurantVariables } from "../../__generated__/restaurant";
 
 const RESTAURANT_QUERY = gql`
@@ -23,6 +24,15 @@ const RESTAURANT_QUERY = gql`
   ${DISH_FRAGMENT}
 `;
 
+const CREATE_ORDER_MUTATION = gql`
+  mutation createOrder($input: CreateOrderInput!) {
+    createOrder(input: $input) {
+      ok
+      error
+    }
+  }
+`;
+
 type SeeRestaurantParams = {
   id: string;
 };
@@ -39,6 +49,14 @@ export const Restaurant = () => {
       },
     }
   );
+  const [orderStarted, setOrderStarted] = useState(false);
+  const [orderItems, setOrderItems] = useState<CreateOrderItemInput[]>([]);
+  const triggerStartOrder = () => {
+    setOrderStarted(true);
+  };
+  const addItemToOrder = (dishId: number) => {
+    setOrderItems((current) => [{ dishId, options: null }]);
+  };
   return (
     <div>
       <Helmet>
@@ -58,17 +76,25 @@ export const Restaurant = () => {
           <h6 className="text-sm font-light">{data?.restaurant.restaurant?.address}</h6>
         </div>
       </div>
-      <div className="container grid mt-16 md:grid-cols-3 gap-x-5 gap-y-10">
-        {data?.restaurant.restaurant?.menu.map((dish, index) => (
-          <Dish
-            key={index}
-            name={dish.name}
-            description={dish.description}
-            price={dish.price}
-            isCustomer={true}
-            options={dish.options}
-          />
-        ))}
+      <div className="container pb-32 flex flex-col items-end mt-20">
+        <button onClick={triggerStartOrder} className="btn px-10">
+          Start Order
+        </button>
+        <div className="w-full grid mt-16 md:grid-cols-3 gap-x-5 gap-y-10">
+          {data?.restaurant.restaurant?.menu.map((dish, index) => (
+            <Dish
+              id={dish.id}
+              orderStarted={orderStarted}
+              key={index}
+              name={dish.name}
+              description={dish.description}
+              price={dish.price}
+              isCustomer={true}
+              options={dish.options}
+              addItemToOrder={addItemToOrder}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
