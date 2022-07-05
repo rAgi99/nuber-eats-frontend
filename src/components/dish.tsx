0d67d7920e88cdb67d14039f6f3bf229dfb7f656
rAgi99 <gunmo2929@naver.com>
2022-07-05 16:12:36 +0900

    Making Order part Three

--- src/components/dish.tsx
+++ src/components/dish.tsx
@@ -9,9 +9,10 @@ interface IDishProps {
   isCustomer?: boolean;
   orderStarted?: boolean;
   options?: restaurant_restaurant_restaurant_menu_options[] | null;
+  isSelected?: boolean;
   addItemToOrder?: (dishId: number) => void;
   removeFromOrder?: (dishId: number) => void;
-  isSelected?: boolean;
+  addOptionToItem?: (dishId: number, option: any) => void;
 }
 
 export const Dish: React.FC<IDishProps> = ({
@@ -25,6 +26,7 @@ export const Dish: React.FC<IDishProps> = ({
   addItemToOrder,
   isSelected,
   removeFromOrder,
+  addOptionToItem,
 }) => {
   const onClick = () => {
     if (orderStarted) {
@@ -38,13 +40,17 @@ export const Dish: React.FC<IDishProps> = ({
   };
   return (
     <div
-      onClick={onClick}
       className={`px-8 pt-4 pb-6 border cursor-pointer transition-all ${
         isSelected ? "border-gray-800" : "hover:border-gray-800"
       }`}
     >
       <div className="mb-5">
-        <h3 className="text-lg font-medium">{name}</h3>
+        <h3 className="text-lg font-medium">
+          {name}{" "}
+          {orderStarted && (
+            <button onClick={onClick}>{isSelected ? "Remove" : "Add"}</button>
+          )}
+        </h3>
         <h4 className="font-medium">{description}</h4>
       </div>
       <span>${price}</span>
@@ -52,7 +58,13 @@ export const Dish: React.FC<IDishProps> = ({
         <div>
           <h5 className="mt-8 mb-3 font-medium">Dish Options:</h5>
           {options?.map((option, index) => (
-            <span className="flex items-center" key={index}>
+            <span
+              onClick={() =>
+                addOptionToItem ? addOptionToItem(id, { name: option.name }) : null
+              }
+              className="flex border items-center"
+              key={index}
+            >
               <h6 className="mr-2">{option.name}</h6>
               <h6 className="text-sm opacity-75">(${option.extra})</h6>
             </span>
